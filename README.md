f8adee807360e80a04fd854ed2ca2cbf76edca4c
Mel Mel Mom <melmelmomof4@gmail.com>
2023-12-15 23:12:40 -0500

    tying out different ways of creating question arrays

--- README.md
+++ README.md
@@ -13,14 +13,16 @@ N/A
 ## Usage
 
 https://melmealey.github.io/Quiz/
-ß
+
 
 
 
 ## Credits
-To style my button
+To style my button:
 https://medium.com/the-clever-dev/change-button-font-size-with-javascript-or-css-2-easy-ways-6cbdd0df9a04
-To learn about countdown timers I watched this video 
+for loops with multiple choice questions:
+https://www.youtube.com/watch?v=LQGTb112N_c
+Learning about countdown timers: 
 https://www.youtube.com/watch?app=desktop&v=x7WJEmxNlEs
 
 
--- script.js
+++ script.js
@@ -1,22 +1,66 @@
 const startButton = document.getElementById("beginQuiz")
 const questionContainterElement = document.getElementById('questionContainer')
-document.addEventListener('click', startGame)
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answerButtons')
 
+// Do I want my questions suffled? let shuffledQuestions, currentQuestionIndex
 
-
+startButton.addEventListener('click', startGame)
 
 function startGame(){
     console.log("Here we go!")
-    // startButton.classList.add('hide')
 }
+    startButton.classList.add('hide')
 
-function nextQuestion(){
+let countdown = 60;
+for (let i=0; i < questions.length; i++)
+    let response =window.prompt();questions[i.prompt]
+    if (response == quesitons [i].answer){
+        alert("Correct!");
+    } else {
+        score--;
+        alert ("NOPE!");
+    }
+
+
+//     shuffledQuestions = questions.sort(() => Math.random() - .5)
+//     currentQuestionIndex = 0
+//     questionContainterElement.classList.remove('hide')
+//     nextQuestion()
+// }
+
  //once the question is answered i wan it to alert Correct! or Wrong! and move on to the next question
-}
+// function nextQuestion(){
+//     showQuestion(shuffledQuestions[currentQuestionIndex])
 
-function chooseAnswer() {
+// }
 
-}
+// function showQuestion(questions) {
+//     questionElement.innerText = questions.questions
+
+// }
+
+
+// function chooseAnswer() {
+
+// }
+
+const questions =  [
+    {
+        prompt: 'What does HTML stand for?\n(a) How To Make Lasagna\n\(b) Hyper Text Markup Language\n(c) Houses Together Move Left\(d) Harry Told Mommy Lies",
+        answer: "b"
+        
+    },
+    {
+
+    },
+    {
+
+    },
+    {,
+    }
+
+]
 
 
 
