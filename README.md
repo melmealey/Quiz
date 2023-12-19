c27c903ebb61197d98ec5712f547cd3c8dcf4265
Mel Mel Mom <melmelmomof4@gmail.com>
2023-12-18 22:26:48 -0500

    proof read and added final formating changes

--- README.md
+++ README.md
@@ -1,21 +1,25 @@
 <Quiz>
 
+## Coding Quiz
+
 <img width="436" alt="Screenshot 2023-12-18 at 3 56 45 PM" src="https://github.com/melmealey/Quiz/assets/147653410/dcb119e2-011e-4d13-a823-7872efd7d2e9">
 
 ## Description
-I think I am finally getting the flow of building a webpage a little better after this project. I learned how to take smaller steps, create 1 variable at a time, add the event Listener to it if it's a button, work on the funtion and for loops that might go with it, and THEN move on to the next variable/ problem I am trying to solve! My tutor was a huge help this week with this process. It's a pretty basic site, CSS could use a lot of work, but it functions as was required and I can improve from here over time. This project pushed me WAY out of my comfort zone. I grew from it!
 
+I think I am finally getting the flow of building a webpage a little better after this project. I learned how to take smaller steps, create 1 variable at a time, add the event Listener to it if it's a button, work on the funtion and for loops that might go with it, and THEN move on to the next variable/ problem I am trying to solve! My tutor was a huge help this week with this process. It's a pretty basic site, CSS could use a lot of work, but it functions as was required and I can improve from here over time. This project pushed me WAY out of my comfort zone. I asked the learning assistant a LOT of questions. I grew from it!
 
 ## Installation
 
 N/A
 
 ## Usage
+
 Test your knowledge of basic coding with this simple Quiz. Type and save your initials for your score to be added to the High Scores Hall of Fame! Try again (as many times as you would like to) to improve your score!
 https://melmealey.github.io/Quiz/
 
 
 ## Credits
+
 To style my button:
 https://medium.com/the-clever-dev/change-button-font-size-with-javascript-or-css-2-easy-ways-6cbdd0df9a04
 for loops with multiple choice questions:
--- script.js
+++ script.js
@@ -3,163 +3,163 @@ const startButton = document.getElementById('beginQuiz')
 const questionContainterElement = document.getElementById('questionContainer')
 const questionElement = document.getElementById('question')
 const answerButtonsElement = document.getElementById('answerButtons')
-const quizContainer =document.getElementById('quizContainer')
-const controls =document.getElementById('controls')
-let time=document.getElementById('time')
-let clockid =""
-const title =document.getElementById('title')
-let answerChoice1 =document.getElementById('choice1')
-let answerChoice2 =document.getElementById('choice2')
-let answerChoice3 =document.getElementById('choice3')
-let answerChoice4 =document.getElementById('choice4')
-let initialContainer =document.getElementById('initialContainer')
-let userScores= JSON.parse(localStorage.getItem("userScores"))||[]
-let initialsBtn =document.getElementById('Initials-btn')
-let initialInput =document.getElementById('initial-input')
-let saveHighScoreContainer =document.getElementById('highScoreContainer')
-let scoreList =document.getElementById('scoreList')
-let tryAgainBtn =document.getElementById('tryAgain')
-let resetHighScores =document.getElementById('resetHighScores')
+const quizContainer = document.getElementById('quizContainer')
+const controls = document.getElementById('controls')
+let time = document.getElementById('time')
+let clockid = ""
+const title = document.getElementById('title')
+let answerChoice1 = document.getElementById('choice1')
+let answerChoice2 = document.getElementById('choice2')
+let answerChoice3 = document.getElementById('choice3')
+let answerChoice4 = document.getElementById('choice4')
+let initialContainer = document.getElementById('initialContainer')
+let userScores = JSON.parse(localStorage.getItem("userScores")) || []
+let initialsBtn = document.getElementById('Initials-btn')
+let initialInput = document.getElementById('initial-input')
+let saveHighScoreContainer = document.getElementById('highScoreContainer')
+let scoreList = document.getElementById('scoreList')
+let tryAgainBtn = document.getElementById('tryAgain')
+let resetHighScores = document.getElementById('resetHighScores')
 
 //Quiz questions array
 let questions = [
     {
-      question: "What does HTML stand for?",
-      answer1: "How To Make Lasagna",
-      answer2: "Hyper Text Markup Language",
-      answer3: "Houses Together Might Levetate",
-      answer4: "Harry Told Mommy Lies",
-      correctAnswer: "Hyper Text Markup Language",
+        question: "What does HTML stand for?",
+        answer1: "How To Make Lasagna",
+        answer2: "Hyper Text Markup Language",
+        answer3: "Houses Together Might Levetate",
+        answer4: "Harry Told Mommy Lies",
+        correctAnswer: "Hyper Text Markup Language",
     },
     {
-      question: "In what year was JavaScript intorduced?",
-      answer1: "1970",
-      answer2: "1997",
-      answer3: "1990",
-      answer4: "1995",
-      correctAnswer: "1995",
+        question: "In what year was JavaScript intorduced?",
+        answer1: "1970",
+        answer2: "1997",
+        answer3: "1990",
+        answer4: "1995",
+        correctAnswer: "1995",
     },
     {
-      question: "JavaScript file has an extnesion of:",
-      answer1: ".Java",
-      answer2: ".javascript",
-      answer3: ".js",
-      answer4: ".xtml",
-      correctAnswer: ".js",
+        question: "JavaScript file has an extnesion of:",
+        answer1: ".Java",
+        answer2: ".javascript",
+        answer3: ".js",
+        answer4: ".xtml",
+        correctAnswer: ".js",
     },
     {
-      question: "Which CSS property controls the text size?",
-      answer1: "text-style",
-      answer2: "font-style",
-      answer3: "text-size",
-      answer4: "font-size",
-      correctAnswer: "font-size",
+        question: "Which CSS property controls the text size?",
+        answer1: "text-style",
+        answer2: "font-style",
+        answer3: "text-size",
+        answer4: "font-size",
+        correctAnswer: "font-size",
     },
     {
-      question: "Which is the correct HTML element for the largest heading?",
-      answer1: "h1",
-      answer2: "head",
-      answer3: "heading",
-      answer4: "h6",
-      correctAnswer: "h1",
+        question: "Which is the correct HTML element for the largest heading?",
+        answer1: "h1",
+        answer2: "head",
+        answer3: "heading",
+        answer4: "h6",
+        correctAnswer: "h1",
     },
-  ];
+];
 
-  let timeRemaining = questions.length *15 
-  let index =0
+let timeRemaining = questions.length * 15
+let index = 0
 
-  function showQuestion() {
+function showQuestion() {
     for (let i = 0; i < questions.length; i++) {
         console.log(questions[i])
-  }
     }
+}
 
 //Here we go! Start Quiz
-function startGame(){
-    startButton.style.display ="none"
+function startGame() {
+    startButton.style.display = "none"
     console.log("Here we go!")
-//start button hides (and the questions container appears) after it is clicked to start the game
+    //start button hides (and the questions container appears) after it is clicked to start the game
     quizContainer.classList.remove("hide")
     controls.classList.add("hide")
-    clockid= setInterval(countdown, 1000)
-    showQuestions () 
+    clockid = setInterval(countdown, 1000)
+    showQuestions()
 }
-function showQuestions () {
-title.textContent = questions [index].question
-answerChoice1.textContent =questions [index].answer1
-answerChoice2.textContent =questions [index].answer2
-answerChoice3.textContent =questions [index].answer3
-answerChoice4.textContent =questions [index].answer4 
+function showQuestions() {
+    title.textContent = questions[index].question
+    answerChoice1.textContent = questions[index].answer1
+    answerChoice2.textContent = questions[index].answer2
+    answerChoice3.textContent = questions[index].answer3
+    answerChoice4.textContent = questions[index].answer4
 
 }
 //alerts
 function checkAnswer(event) {
-    let guess=event.target.textContent
-  const answer = questions[index].correctAnswer;
-  if (guess === answer) {
-    //Display "CORRECT!"
-    alert("CORRECT!");
-  } else {
-    // Display "NOPE!"
-    alert("NOPE! -10 seconds!");
-    timeRemaining=timeRemaining-10;
+    let guess = event.target.textContent
+    const answer = questions[index].correctAnswer;
+    if (guess === answer) {
+        //Display "CORRECT!"
+        alert("CORRECT!");
+    } else {
+        // Display "NOPE!"
+        alert("NOPE! -10 seconds!");
+        timeRemaining = timeRemaining - 10;
 
-  }
-  index++;
-  if (index< questions.length){
-    showQuestions();
-  }
-else {
-quizContainer.classList.add("hide")
-initialContainer.classList.remove("hide")
-tryAgainBtn.classList.remove("hide")
-resetHighScores.classList.remove("hide")
-    
-    stopTimer()
-}
-//Timer ends at 0 seconds
+    }
+    index++;
+    if (index < questions.length) {
+        showQuestions();
+    }
+    else {
+        quizContainer.classList.add("hide")
+        initialContainer.classList.remove("hide")
+        tryAgainBtn.classList.remove("hide")
+        resetHighScores.classList.remove("hide")
+
+        stopTimer()
+    }
+    //Timer ends at 0 seconds
 }
 
-function stopTimer () {
+function stopTimer() {
     clearInterval(clockid)
 }
 
-function countdown () {
-    time.textContent= timeRemaining--
+function countdown() {
+    time.textContent = timeRemaining--
     if (time === 0) {
         stopTimer();
-      }
+    }
 }
 //Save and display scores
-function saveHighScore(score){
-    const user={
-        initial:initialInput.value ,
-        score:time.textContent
+function saveHighScore(score) {
+    const user = {
+        initial: initialInput.value,
+        score: time.textContent
     }
     userScores.push(user)
-    localStorage.setItem("userScores",JSON.stringify(userScores));
-    initialContainer.classList.add ("hide");
+    localStorage.setItem("userScores", JSON.stringify(userScores));
+    initialContainer.classList.add("hide");
     saveHighScoreContainer.classList.remove("hide");
     displayHighScore()
 }
-function displayHighScore () {
- scoreList.textContent=""
- for (let i = 0; i < userScores.length; i++) {
-   var li = document.createElement("li")
-   li.textContent=userScores[i].initial+" - "+ userScores[i].score
-   scoreList.appendChild(li);
-    
- }
+function displayHighScore() {
+    scoreList.textContent = ""
+    for (let i = 0; i < userScores.length; i++) {
+        var li = document.createElement("li")
+        li.textContent = userScores[i].initial + " - " + userScores[i].score
+        scoreList.appendChild(li);
+
+    }
 }
 tryAgainBtn.addEventListener("click", function () {
     location.reload();
-  });
+});
 
-  resetHighScores.addEventListener("click", function () {
+resetHighScores.addEventListener("click", function () {
     userScores = [];
     localStorage.removeItem("userScores");
     displayHighScore(); // Update the high score display
-  });
+});
 
 
 startButton.addEventListener('click', startGame)
@@ -171,36 +171,3 @@ answerButtonsElement.addEventListener("click", checkAnswer)
 initialsBtn.addEventListener("click", saveHighScore)
 tryAgainBtn.addEventListener("click", tryAgainBtn)
 resetHighScores.addEventListener("click", resetHighScores)
-
-
-
-
-
-
-// //Random word generator
-// let randomIndex = Math.floor(Math.random () * wordBank.length);
-
-// //Start with blanks with correct amount of letters for word
-// let start ="";
-// for (let i = 0; i < start.length; i++) {
-//     start += "_";
-
-// }
-// const startingMinutes = 10
-// let time = startingMinutes * 60;
-
-// const countdownEl = document.getElementById('countdown');
-
-// setInterval(updateCountdown, 1000);
-
-// function updateCountdown(){
-//     const minutes = Math.floor(time / 60);
-//     let seconds = time % 60;
-
-//     seconds = seconds <10 ? '0' +seconds : seconds;
-
-//     countdownEl.innerHTML = '${minutes}:${seconds}';
-//     time--;
-
-// }
-
